8040e0383da764e8775df6b7b19313a1423688a8
Javier Lima <javi.limashelby@gmail.com>
2024-08-26 14:58:09 -0400

    feat: Add Shuffled frog leaping algorithm

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from alg import Example
 from bat_algorithm import Bat
 from front_plot import *
-from bounded_function import rotated_elipse2, scahffer3, ackley, beale
+from bounded_function import rotated_elipse2, scahffer3, ackley, beale, schaffer2
 from particle_swarm import ParticleSwarm
+from sfla import SFLA
 
 """
 call this function passing an Algoritm and a Function to Test.
 """
-front_plot(Bat(60), beale)
+# front_plot(Bat(60), beale)
+
+front_plot(SFLA(
+    name='Schaffer No. 2', 
+    objective_function=schaffer2.f, 
+    m=100, 
+    n=50,
+    dimension=2,
+    N=10, 
+    q=10, 
+    S=100
+    ), schaffer2)
