b1ec954a76261282865439466e9b38c92a88a155
BritoAlv <gomzalezbrito@gmail.com>
2024-08-28 17:12:47 -0400

    feat : add alg vs alg plot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bat_algorithm import Bat
-from front_plot import *
+from plots import *
 from bounded_function import rotated_elipse2, scahffer3, ackley, beale, schaffer2, ripple25, booth
 from gradient_descent import GradientDescent
 from particle_swarm import ParticleSwarm
@@ -8,4 +8,4 @@ from sfla import SFLA
 """
 call this function passing an Algoritm and a Function to Test.
 """
-front_plot(Bat(60, booth.range_x1, booth.range_x2), booth)
+two_plot(Bat(20, booth.range_x1, booth.range_x2), ParticleSwarm(20, booth.range_x1, booth.range_x2), booth)
---
+++ plots.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+from matplotlib.animation import FuncAnimation
+from bounded_function import BoundedFunction
+from alg import Alg
+
+def one_plot( alg : Alg, bf : BoundedFunction):
+    # Initial setup
+    x = np.linspace(bf.range_x1[0], bf.range_x1[1])
+    y = np.linspace(bf.range_x1[0], bf.range_x2[1])
+    X, Y = np.meshgrid(x, y)
+    # Compute the function values
+    Z = bf.f(X, Y)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    contour = ax.contourf(X,Y,Z, levels=10, cmap = "plasma")
+    scatter = ax.scatter([], [] , color='r', s = 10, zorder = 10)
+    ax.set_title(f"Interactive Plot of Algorithm {alg.name} with Function {bf.name}")
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    colorbar = plt.colorbar(contour, ax=ax)
+    colorbar.set_label('Function Value')
+
+    # Function to update scatter data
+    def update_scatter():
+        alg.update_points(bf)
+        points = alg.get_points()
+        scatter_data = np.array([[p[0] for p in points], [p[1] for p in points]])
+        scatter.set_offsets(scatter_data.T)
+        alg.log_state(bf)
+
+    # Button callback function
+    def button_callback(event):
+        update_scatter()
+        fig.canvas.draw_idle()
+
+    # Add button widget
+    ax_button = plt.axes([0.7, 0.05, 0.2, 0.075])
+    button = Button(ax_button, 'Update Scatter')
+    button.on_clicked(button_callback)
+
+
+    # Show the plot
+    plt.show()
+
+
+def two_plot(alg1 : Alg, alg2 : Alg, bf : BoundedFunction):
+    # Initial setup
+    x = np.linspace(bf.range_x1[0], bf.range_x1[1])
+    y = np.linspace(bf.range_x1[0], bf.range_x2[1])
+    X, Y = np.meshgrid(x, y)
+    # Compute the function values
+    Z = bf.f(X, Y)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    contour = ax.contourf(X,Y,Z, levels=10, cmap = "plasma")
+    scatter1 = ax.scatter([], [] , color='r', s = 10, zorder = 10)
+    scatter2 = ax.scatter([], [], color = 'b', s = 10, zorder = 10)
+    ax.set_title(f"Interactive Plot of Algorithm {alg1.name} vs {alg2.name} with Function {bf.name}")
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    colorbar = plt.colorbar(contour, ax=ax)
+    colorbar.set_label('Function Value')
+
+    # Function to update scatter data
+    def update_scatter():
+        for alg, scatter in zip([alg1, alg2], [scatter1, scatter2]):   
+            alg.update_points(bf)
+            points = alg.get_points()
+            scatter_data = np.array([[p[0] for p in points], [p[1] for p in points]])
+            scatter.set_offsets(scatter_data.T)
+            alg.log_state(bf)
+
+    # Button callback function
+    def button_callback(event):
+        update_scatter()
+        fig.canvas.draw_idle()
+
+    # Add button widget
+    ax_button = plt.axes([0.7, 0.05, 0.2, 0.075])
+    button = Button(ax_button, 'Update Scatter')
+    button.on_clicked(button_callback)
+
+
+    # Show the plot
+    plt.show()
